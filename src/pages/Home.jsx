17fc37c801fbaa6ca96b247ff081521e731b1ed1
Frogalove <143967544+Frogalove@users.noreply.github.com>
2024-01-22 16:06:29 +0100

    add file

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,8 @@ function Home() {
     <>
       <h1>Homepagina</h1>
       <section>
+        <p>
+        </p>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem id libero provident! Accusantium at autem
           cumque, esse eum, fugiat ipsa ipsam quia quis ratione repudiandae sapiente sequi sit tempora voluptates.
           Accusantium aperiam asperiores, beatae dolor eius error expedita laboriosam maiores minima mollitia nisi
